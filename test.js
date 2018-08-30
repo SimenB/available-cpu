eeee62c1acb8c8a610e5ffd528c4d77b7d9fe958
simbekkh <simen.bekkhus@finn.no>
2018-08-30 17:34:05 +0200

    test with physical-cpu-count

--- test.js
+++ test.js
@@ -2,5 +2,6 @@
 
 const os = require('os');
 const res = require('./')();
+const physicalCpuCount = require('physical-cpu-count');
 
-console.log({ res, os: os.cpus().length });
+console.log({ res, os: os.cpus().length, physicalCpuCount });
